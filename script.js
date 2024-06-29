68b3125d72351a8c45d86edd4683c6eacaf126ff
Valerii Kharchenko <vkharchenko2272@gmail.com>
2024-06-28 20:27:22 -0500

    API example with script file added

---
+++ script.js
@@ -0,0 +1,17 @@
+async function getsapi() {
+
+
+    var url = '';
+
+    var response = await fetch(url);
+    var result = await response.json();
+
+    var myName = document.getElementById("personaname");
+
+  
+}
+
+var name = getsapi();
+
+//a find element to modifi 
+//change element in span to 
