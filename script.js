0035fd5eec7684b5ddbcec1f3730295c44c6e90d
Valerii Kharchenko <vkharchenko2272@gmail.com>
2024-07-03 15:31:33 -0500

    API connected and basic information showed up

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 "use strict";
 
-await fetch("")
+fetch("")
 .then((result)=>{
     return result.json()
 }).then((data)=>{
     console.log(data)
-    let videos = data.items
-    nextPageToken = data.nextPageToken
+    let videos = data.items;
+    let nextPageToken = data.nextPageToken;
+    console.log(nextPageToken);
     let videoContainer = document.querySelector(".right-section")
-    for(video of videos){
+    for(let video of videos){
         videoContainer.innerHTML += `
-            <img src = "$video.snippet.thumbnails.default.url">
+            <img src = "${video.snippet.thumbnails.default.url}">
         `
     }
 })
