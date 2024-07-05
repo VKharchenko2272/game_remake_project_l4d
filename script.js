b70e9385935dce5e3d5723956607db3d7c9170db
Valerii Kharchenko <vkharchenko2272@gmail.com>
2024-07-04 19:23:08 -0500

    youtube api connected, 1 bug there with a video

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-"use strict";
+const API_URL = ``;
+let nextPageToken = '';
 
-fetch("")
-.then((result)=>{
-    return result.json()
-}).then((data)=>{
-    console.log(data)
-    let videos = data.items;
-    let nextPageToken = data.nextPageToken;
-    console.log(nextPageToken);
-    let videoContainer = document.querySelector(".right-section")
-    for(let video of videos){
-        videoContainer.innerHTML += `
-            <img src = "${video.snippet.thumbnails.default.url}">
-        `
+function fetchVideos(pageToken = '') {
+    let url = API_URL;
+    if (pageToken) {
+        url += `&pageToken=${pageToken}`;
     }
-})
+
+    fetch(url)
+        .then(result => result.json())
+        .then(data => {
+            console.log(data);
+            let videos = data.items;
+            nextPageToken = data.nextPageToken;
+            console.log(nextPageToken);
+            let videoContainer = document.querySelector(".right-section");
+            for (let video of videos) {
+                const videoId = video.id.videoId;
+                const videoTitle = video.snippet.title;
+                console.log(video.snippet.publishTime);
+                videoContainer.innerHTML += `
+                            <div class="video-item">
+                                <iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+                                <h3>${videoTitle}</h3>
+                            </div>
+                        `;
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching YouTube API:', error);
+        });
+}
+
+// Future
+// fetchVideos();
+
+
+// document.getElementById('load-more').addEventListener('click', () => {
+//     if (nextPageToken) {
+//         fetchVideos(nextPageToken);
+//     }
+// });
